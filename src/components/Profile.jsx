b3604a92a968315e73b5e485b5d5e8234bce7ff2
Evgen Matysevich <evgenmatysevich@ukr.net>
2020-08-26 23:22:23 +0300

    test commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+  return <div className='content'>
+    <div>
+       <img src='https://lh3.googleusercontent.com/proxy/Lg5ezfBoFi6Wv0WpR5q0wjEp6rZyjPs0i2S5EB_x9R9nzy3j12LBQXbAhUIoz1ZgVPeP25JAY3vEBseGbhiYFY7VpCZ82irMXgoRg_W5Itb5ouXCBEdKBaqGqk1UqiekyS55cfRmH9rUjwLb_7iEZwPTFi3uBqSmZBpARmTVYQ' />
+    </div>
+       <div>
+       ava + description
+       </div>
+
+       <div>
+       My post
+         <div>
+       New post
+       </div>
+       <div>
+         <div>
+       post 1
+        </div>
+         <div>
+       post 2
+        </div>
+     </div>
+    </div>
+  </div>
+
+}
+
+export default Profile;
