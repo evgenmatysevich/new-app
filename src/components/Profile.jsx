f9dbd3575f3ec61d1e2c7a2585412b635092e133
Evgen Matysevich <evgenmatysevich@ukr.net>
2020-08-31 19:51:23 +0300

    test 3

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Profile = () => {
   return <div className='content'>
     <div>
-       <img src='https://lh3.googleusercontent.com/proxy/Lg5ezfBoFi6Wv0WpR5q0wjEp6rZyjPs0i2S5EB_x9R9nzy3j12LBQXbAhUIoz1ZgVPeP25JAY3vEBseGbhiYFY7VpCZ82irMXgoRg_W5Itb5ouXCBEdKBaqGqk1UqiekyS55cfRmH9rUjwLb_7iEZwPTFi3uBqSmZBpARmTVYQ' />
+       <img src='https://lh3.googleusercontent.com/proxy/fQPe9SatwRm-ZADV44Pey_FWwaW-WZVTTkT9qBPjpHksjwFUS5Viz-lW49anHUWburvWy2ddQibI8SNKwQwCusxBrby-T-PLw2Do_CbPRyjVPVyv0J2i_kiN' />
     </div>
        <div>
        ava + description
