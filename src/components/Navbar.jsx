f802de567958b03f7aaab687caea6d920f6ee73e
Evgen Matysevich <evgenmatysevich@ukr.net>
2020-08-31 20:08:32 +0300

    new css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+ import React from 'react';
+ import './Navbar.css';
 
 const Navbar = () => {
 	return <nav className='nav'>
-	<div>Profile</div>
-	<div><a>Messages</a></div>
-	<div><a>News</a></div>
-	<div><a>Music</a></div>
-	<div><a>Books</a></div>
-	<div><a>Settings</a></div>
+	<div className='item'>Profile</div>
+	<div className='item'><a>Messages</a></div>
+	<div className='item'><a>News</a></div>
+	<div className='item'><a>Music</a></div>
+	<div className='item'><a>Books</a></div>
+	<div className='item'><a>Settings</a></div>
 	</nav>
 }
 export default Navbar;
