a5a37407a11318b98040af813a46564e1e0c182c
Evgen Matysevich <evgenmatysevich@ukr.net>
2020-09-03 21:47:28 +0300

    href

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   <div className='item'><a href="">Messages</a></div>
   <div className='item'><a href="">News</a></div>
   <div className='item'><a href="">Music</a></div>
-  <div className='item'><a href="">Books</a></div>
+  <div className='item'><a href="https://www.ukr.net/">Books</a></div>
   <div className='item'><a href="">Settings</a></div>
   </nav>
 }
