9bb55acd0beaae282670c7680758ddac45dbf1bb
Evgen Matysevich <evgenmatysevich@ukr.net>
2020-10-28 20:19:11 +0200

    28/10/2020

--- src/components/Seting/Seting.js
+++ src/components/Seting/Seting.js
@@ -1,7 +1,26 @@
 import React from "react";
 import "./Seting.css";
 
-const Seting = (props) => {
-  return <div className="seting">Settings</div>;
+
+const API_KEY = "911128e937701fa5a782638ef8b411cf";
+
+
+
+class Seting extends React.Component {
+  gettingWeather = async () => {
+    const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Kiev&appid=${API_KEY}&units=metric`);
+    const data = await api_url.json();
+    console.log(data);
+  }
+  render() {
+    return (
+      <div className="seting">
+        <form>
+          <input type="text" name="city" placeholder="Город" />
+          <button >Получить погоду</button>
+        </form>
+      </div>
+    );
+  }
 };
 export default Seting;
